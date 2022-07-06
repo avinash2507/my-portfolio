9aab9e64786978db10ad60694bf4d670be07959f
aviash2507 <avinash2632@gmail.com>
2022-07-06 18:09:32 +0530

    minor change

--- src/Contant/Const.js
+++ src/Contant/Const.js
@@ -4,8 +4,8 @@ export const portfolios = [
         id: 1,
         category: 'Reactjs',
         image: './assets/screen.png',
-        github: 'https://github.com/avinash2507',
-        linkedin: 'https://narendra224.herokuapp.com/home',
+        github: 'https://github.com/avinash2507/my-portfolio',
+        linkedin: 'https://my-portfolio-qjwaveqdw-avinash2507.vercel.app/home',
         title: 'OPEN SOURCE,Reactjs Portfolio webApp',
         text: 'Libraries:React,React-Hooks, Styled-components, Material-Ui',
     }
